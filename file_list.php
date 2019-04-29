ef155cebf757d9eb9a5afc70c5ccf9d47d5bb8ba
Richard Bramley <rabramley@gmail.com>
2019-04-29 11:13:54 +0100

    File list template

---
+++ file_list.php
@@ -0,0 +1,28 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+<@ snippets/header.php @>
+
+<div>
+	<section>
+		<h2>@{ title }</h2>
+		<p>@{ text_introduction }</p>
+	</section>
+
+	<div class="card">
+		<div class="card-header"></div>
+		<div class="card-body">
+			@{ text_main }
+		</div>
+		<ul>
+			<@ filelist { glob: @{ files } } @>
+
+			<@ foreach in filelist @>
+				<li>
+					<a href="@{ :file }">@{ :caption | stripTags }</a>
+				</li>
+			<@ end @>
+		</ul>
+	</div>
+</div>
+	
+<@ snippets/javascript.php @>
+<@ snippets/footer.php @>
