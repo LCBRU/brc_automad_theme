8d13effa7e72935d7cc1cb1a449e34588cb4a353
Richard Bramley <rabramley@gmail.com>
2020-12-01 13:31:21 +0000

    Correct parameters

--- system.php
+++ system.php
@@ -1,11 +1,10 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!');?>
-<h2><a name="@{ name }" href="@{url}">@{ title }</a></h2>
-@{ order }
+<h2><a class="@{visibility}" href="@{url}">@{ title }</a></h2>
 <h3>Description</h3>
 <p>
     @{ text }
 </p>
-<a class="@{class}" href="@{dpia_url}">DPIA</a>
+<a href="@{dpia_url}">DPIA</a>
 <h3>Protocol Statement</h3>
 <p>
     @{ protocol_statement }
