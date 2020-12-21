f86a73425516f14230647d22da4569fd8845b081
Richard Bramley <rabramley@gmail.com>
2020-12-21 15:24:28 +0000

    Default sidebar

--- snippets/footer.php
+++ snippets/footer.php
@@ -3,50 +3,6 @@
 			</div>
 			<div class="col-lg-3">
 				@{ side_bar }
-				<section>
-
-					<h3>IT Helpdesks</h3>
-
-					<hr>
-
-					<h4>UHL</h4>
-
-					<dl>
-						<dt>Tel</dt>
-						<dd>0116 258 8000 (#8000)</dd>
-						<dt>Email</dt>
-						<dd><a href="mailto:sdrequests@uhl-tr.nhs.uk">sdrequests@uhl-tr.nhs.uk</a></dd>
-						<dt>Self Service</dt>
-						<dd><a href="http://servicedesk.xuhl-tr.nhs.uk/">UHL IT Servicedesk</a></dd>
-					</dl>
-
-					<h4>UoL</h4>
-
-					<dl>
-						<dt>Tel</dt>
-						<dd>0116 252 2253</dd>
-						<dt>Email</dt>
-						<dd><a href="mailto:ithelp@le.ac.uk">ithelp@le.ac.uk</a></dd>
-						<dt>Self Service</dt>
-						<dd><a href="https://ithelp.le.ac.uk/webdesk.webaccess/">Self Service Website</a></dd>
-						<dt>Documentation</dt>
-						<dd><a href="https://www2.le.ac.uk/offices/itservices/ithelp/help/it-service-desk">IT Service Desk Webpage</a></dd>
-						<dt>Department IT Contact (DITC)</dt>
-						<dd><a href="https://uniofleicester.sharepoint.com/sites/staff/representative-roles/departmental-it-contact/SitePages/ditc-list.aspx">DITCs</a></dd>
-					</dl>
-
-					<h4>BRC IT</h4>
-
-					<dl>
-						<dt>Email</dt>
-						<dd><a href="mailto:richard.a.bramley@uhl-tr.nhs.uk">Richard Bramley</a></dd>
-					</dl>
-				</section>
-
-				<section>
-					<a class="twitter-timeline" data-tweet-limit="3" href="https://twitter.com/LeicesterBRC?ref_src=twsrc%5Etfw">Tweets by LeicesterBRC</a>
-					<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-				</section>
 			</div>
 
 		</div>
